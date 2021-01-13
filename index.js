e87289580097f38660c4f126def2d928eb456cbf
Andrzej Zabski <jedrak32@gmail.com>
2021-01-13 21:26:09 +0100

    basic discor bot

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//change
+const Discord = require('discord.js');
+
+require('dotenv').config();
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.BOT_TOKEN);
