ec6b7577429047f4c045e0b90377b23d2b800452
Andrzej Zabski <jedrak32@gmail.com>
2021-01-13 22:20:38 +0100

    Commando hello world

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const Discord = require('discord.js');
-
 require('dotenv').config();
-const client = new Discord.Client();
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
 
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+const client = new CommandoClient({
+	commandPrefix: '?',
+	owner: '278305350804045834',
+	invite: 'https://discord.gg/bRCvFy9',
 });
+
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		['first', 'Your First Command Group'],
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+	client.user.setActivity('with Commando');
+});
+
+client.on('error', console.error);
 
 client.login(process.env.BOT_TOKEN);
